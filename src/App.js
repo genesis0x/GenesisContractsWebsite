555050e84c62b2d6860810a37a5ed7e8f333fb33
genesis0x <80225206+genesis0x@users.noreply.github.com>
2021-03-11 21:01:08 -0600

    added background image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import { Platforms } from "./Platforms";
+import background from "./images/gc_background.png";
 
 function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
@@ -7,11 +8,24 @@ function Welcome(props) {
 
 export default function App() {
   return (
-    <div className="App">
+    <div
+      className="App"
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundSize: "auto",
+        backgroundRepeat: "repeat"
+      }}
+    >
       <h1>Genesis Contracts</h1>
       <h2>Smart Contract - Platform Comparison 4</h2>
       <Welcome name="Chris Pelling" />
       <Platforms />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
