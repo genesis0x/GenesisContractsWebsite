fb8d4af95155a2e76286869942f5234a5036b715
shotguncrypto <80225206+shotguncrypto@users.noreply.github.com>
2021-04-18 12:43:13 -0500

    another test of commits / deploys

--- src/platformData.js
+++ src/platformData.js
@@ -1,7 +1,7 @@
 export const platformDataRows = [
   {
     id: 1,
-    company: "Twitter Inc 2",
+    company: "Twitter Inc 3",
     ticker: "TWTR",
     stockPrice: "22.76 USD",
     timeElapsed: "5 sec ago"
